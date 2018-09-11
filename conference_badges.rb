4de8c16b1381fbda44959a6441ce1b06e5679c75
John Werenski <jtwerenski@gmail.com>
2018-09-11 15:16:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ def assign_rooms(array)
   end 
 end
 
-def printer(batch_badge_creator(), assign_rooms())
+def printer(batch_badge_creator, assign_rooms)
   batch_badge_creator.each do |position|
     puts position
   end 
