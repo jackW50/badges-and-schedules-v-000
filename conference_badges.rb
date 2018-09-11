42f7bdeab251ff195392a5f067a9f84976ef8b60
John Werenski <jtwerenski@gmail.com>
2018-09-11 15:15:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+  greeting = "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(array)
+  array.collect do |name|
+    badge_maker(name)
+  end 
+end 
+
+def assign_rooms(array)
+  counter = 0
+  array.collect do |speaker|
+    counter += 1 
+    room_assignment = "Hello, #{speaker}! You'll be assigned to room #{counter}!"
+  end 
+end
+
+def printer()
+  array.each do |position|
+    puts position
+  end 
+  array_2.each do |num|
+    puts num 
+  end 
+end 
